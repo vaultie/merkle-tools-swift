3e650e3d4419e9a0efc96b782cb88efc4ac34333
Mykyta Plakhotin <nikita.plakhotin@gmail.com>
2019-12-13 12:31:51 -0500

    Initial project setup

---
+++ MerkleTools/MerkleTools.h
@@ -0,0 +1,19 @@
+//
+//  MerkleTools.h
+//  MerkleTools
+//
+//  Created by Nikita Plakhotin on 2019-12-13.
+//  Copyright © 2019 Vaultie Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MerkleTools.
+FOUNDATION_EXPORT double MerkleToolsVersionNumber;
+
+//! Project version string for MerkleTools.
+FOUNDATION_EXPORT const unsigned char MerkleToolsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MerkleTools/PublicHeader.h>
+
+
